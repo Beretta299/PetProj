2840ab4b36144bf0c941123bc4d0a508a73d47d1
Beretta29992 <dmitry.gerasimenko@bas-ip.com>
2024-05-07 16:30:23 +0200

    Swipe card incomplete

--- app/src/main/java/com/karas/petproj/ui/components/MainScreenComponent.kt
+++ app/src/main/java/com/karas/petproj/ui/components/MainScreenComponent.kt
@@ -1,6 +1,10 @@
 package com.karas.petproj.ui.components
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,18 +13,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,7 +47,6 @@ import com.karas.petproj.main.MainViewModel
 
 @Composable
 fun MainScreenComponent(viewModel: MainViewModel) {
-
     val swipeAbleUsers = viewModel.userSwipedImage
     for(itemIndex in 0..<swipeAbleUsers.size) {
         val item = swipeAbleUsers.get(itemIndex)
@@ -56,7 +70,42 @@ fun MainScreenComponent(viewModel: MainViewModel) {
             }
         }
     }
+}
 
+@Composable
+@Preview(showSystemUi = true)
+fun ShowItemCard() {
+    var expanded by remember { mutableStateOf(false) }
+    val offsetY by animateFloatAsState(targetValue = if (expanded) -100f else 0f)
+
+    Row(modifier = Modifier
+        .padding(8.dp)
+        .fillMaxHeight()
+        .clickable {
+            expanded = !expanded
+        }
+        .background(Color.Black, shape = RoundedCornerShape(22.dp)), verticalAlignment = Alignment.Top) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()) {
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(painter = painterResource(id = R.drawable.imgag),
+                    modifier = Modifier
+                        .animateContentSize()
+                        .fillMaxWidth()
+                        .height(if (expanded) 700.dp else 300.dp)
+                        .clip(RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp)).graphicsLayer { translationY = offsetY }, contentScale = ContentScale.FillBounds, contentDescription = "")
+            }
+            CardBigTitle(text = "NAME")
+            CardSmallTitle(text = "BalinaAAS:DA:SDA:D:A:DABalinaAAS:DA:SDA:D:A:DABalinaAAS:DA:SDA:D:A:DABalinaAAS:DA:SDA:D:A:DABalinaAAS:DA:SDA:D:A:DABalinaAAS:DA:SDA:D:A:DA", modifier = Modifier.padding(8.dp, 0.dp, 0.dp))
+            Spacer(modifier = Modifier
+                .fillMaxWidth()
+                .height(8.dp))
+            CategoryDivider(iconId = R.drawable.outline_brightness_1_24, text = "Activities")
+        }
+    }
 }
 
 @Composable
@@ -78,10 +127,7 @@ fun ItemCard(swipeAbleUser: SwipeAbleUser = SwipeAbleUser(null, "John DOE","http
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(700.dp), contentScale = ContentScale.FillBounds, contentDescription = "")
-
-
             }
-
             CardBigTitle(text = swipeAbleUser.name)
             CardSmallTitle(text = "Balina", modifier = Modifier.padding(8.dp, 0.dp, 0.dp))
             Spacer(modifier = Modifier
@@ -91,19 +137,33 @@ fun ItemCard(swipeAbleUser: SwipeAbleUser = SwipeAbleUser(null, "John DOE","http
     }
 }
 
-@Preview(showSystemUi = true)
 @Composable
 fun ItemCardPreview() {
     ItemCard()
 }
 
-@Preview(showSystemUi = true)
 @Composable
 fun CardBigTitle(text: String = "", color: Color = Color.White) {
     Text(text = text, color = color, modifier = Modifier.padding(8.dp, 0.dp, 0.dp,0.dp), fontSize = 22.sp, fontWeight = FontWeight.Bold)
 }
-@Preview(showSystemUi = true)
 @Composable
 fun CardSmallTitle(text: String = "", color: Color = Color.White, modifier: Modifier = Modifier) {
-    Text(text = text, color = color, modifier = modifier, fontSize = 18.sp)
+    Text(text = text, color = color, modifier = modifier, fontSize = 18.sp, overflow = TextOverflow.Ellipsis)
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun CategoryDivider(iconId: Int = R.drawable.baseline_bluetooth_disabled_24,text: String = "AAAA", color: Color = Color.White) {
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)) {
+        Icon(imageVector = Icons.Filled.Favorite, modifier = Modifier.padding(0.dp, 0.dp,8.dp, 0.dp), tint = Color.White, contentDescription = "")
+        Spacer(modifier = Modifier
+            .background(Color.Gray)
+            .width(16.dp)
+            .height(2.dp))
+        Text(text = text, color = color, modifier = Modifier.padding(8.dp, 0.dp, 8.dp, 0.dp), fontSize = 18.sp, overflow = TextOverflow.Ellipsis)
+        Spacer(modifier = Modifier
+            .background(Color.Gray)
+            .weight(1f)
+            .height(2.dp))
+    }
 }
